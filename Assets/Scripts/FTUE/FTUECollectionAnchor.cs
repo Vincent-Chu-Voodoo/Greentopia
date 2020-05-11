3af4277bf649cf40af7c33cc1096e8e1324ba49e
vincent <vincentc@voodoo.com>
2020-05-11 17:53:59 +0200

    refinement

---
+++ Assets/Scripts/FTUE/FTUECollectionAnchor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTUECollectionAnchor : MonoBehaviour
+{
+    public float maxRadius;
+    public List<Transform> movedWithAnchorList;
+    public List<Transform> movedWithAnchorList2;
+
+    public void OnMoved()
+    {
+        return;
+        var wp = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 8f));
+        var delta = wp - transform.position;
+        delta = delta.normalized * maxRadius;
+        transform.Translate(delta);
+        foreach (var t in movedWithAnchorList)
+            t.Translate(delta);
+        foreach (var t in movedWithAnchorList2)
+            t.Translate(2f * delta);
+        print($"{wp} {delta}");
+    }
+}

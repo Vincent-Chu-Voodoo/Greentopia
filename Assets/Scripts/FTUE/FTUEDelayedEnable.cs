f362335ca26eb4de555e2fda737fc49a3d15c786
vincent <vincentc@voodoo.com>
2020-05-11 18:38:51 +0200

    tweaking the delay

---
+++ Assets/Scripts/FTUE/FTUEDelayedEnable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTUEDelayedEnable : MonoBehaviour
+{
+    public float delay;
+
+    public GameEvent OnEnable;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(delay);
+        OnEnable.Invoke(this);
+    }
+}
